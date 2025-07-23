45ac182dc03cad01dc14283f6e8f6cff85fe62a4
Marcjannaa <32717296+Marcjannaa@users.noreply.github.com>
2025-07-23 18:16:59 +0200

    Unity Version Control deleted
    Miałeś włączoną kontrolę wersji Unity, nie potrzebujesz jej bo mamy już do tego GitHuba. Z jakiegoś powodu dawała błędy więc ją usunęłam

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    
+    
+    
     private void Awake()
     {
         if (Instance == null)
